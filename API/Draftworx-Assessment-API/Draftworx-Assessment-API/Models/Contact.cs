5bea597a681b942418fd9ef4d7d37abb8978f41f
Ruan Gates <ruangates@gmail.com>
2024-06-02 22:07:32 +0200

    Updating test data. Edited model to use camelCase properties because my VS linter keeps enforcing that.

--- API/Draftworx-Assessment-API/Draftworx-Assessment-API/Models/Contact.cs
+++ API/Draftworx-Assessment-API/Draftworx-Assessment-API/Models/Contact.cs
@@ -2,11 +2,11 @@
 {
   public class Contact
   {
-    public int Id { get; set; }
-    public string ContactName { get; set; }
-    public string PhoneNumber { get; set; }
-    public string BestTimeToContact { get; set; }
-    public string ReasonForCall { get; set; }
-    public List<string> Notes { get; set; }
+    public int id { get; set; }
+    public string contactName { get; set; }
+    public string phoneNumber { get; set; }
+    public string? bestTimeToContact { get; set; }
+    public string? reasonForCall { get; set; }
+    public List<string>? notes { get; set; }
   }
 }
